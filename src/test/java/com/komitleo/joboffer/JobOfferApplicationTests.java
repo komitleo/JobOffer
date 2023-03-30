dcc408063a6f937577b6ac424b498351aee8a540
Komitleo-PC <jacek@komitleo.pl>
2023-03-30 21:18:55 +0200

    Initial commit

---
+++ src/test/java/com/komitleo/joboffer/JobOfferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komitleo.joboffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOfferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
